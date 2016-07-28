9f2ab3d874e64e9d373e31cb04f7ac34f31022e5
weiqifa <weiqifa0@qq.com>
2016-07-28 10:47:43 +0800

    [问题描述]:新来的纸片样张打印设置 [评审人]:韦启发 	modified:   QT5.4xiaoming/serial_qrencode/build-serial_qrencode-Qt5_4_1Static-Release/bin/Serial_qrencode.exe 	modified:   QT5.4xiaoming/serial_qrencode/build-serial_qrencode-Qt5_4_1Static-Release/bin/macAdress.txt 	modified:   QT5.4xiaoming/serial_qrencode/serial_qrencode/mainwindow.h

--- QT5.4xiaoming/serial_qrencode/serial_qrencode/mainwindow.h
+++ QT5.4xiaoming/serial_qrencode/serial_qrencode/mainwindow.h
@@ -74,13 +74,13 @@ private:
      * *************************************/
     #define D_MARGIN_VALUE 0
     #define D_DPI 25.5 //毫米和像素的转换
-    #define D_PARAMETER 3.775
-    #define D_TWODIMENSION_SIZE 10*D_PARAMETER //定义打印二维码图像大小
-    #define D_LEFT_MARGIN 2.29*D_PARAMETER  //左边距2.29mm
-    #define D_UP_MARGIN 1.0*D_PARAMETER
-    #define D_TWODIMENSION_INTERVAL 3.58*D_PARAMETER
+    #define D_PARAMETER 3.775//差值，乘以这个差值才是正确的mm
+    #define D_TWODIMENSION_SIZE 12*D_PARAMETER //定义打印二维码图像大小
+    #define D_LEFT_MARGIN 1.0*D_PARAMETER  //左边距2.29mm
+    #define D_UP_MARGIN 0.7*D_PARAMETER //上边距
+    #define D_TWODIMENSION_INTERVAL 5.0*D_PARAMETER //左右二维码的中间间隔
     #define D_PITURE_B_TEXT 1.7*D_PARAMETER //二维码和文字的距离
-    #define D_TEXT_SIZE 4//字体大小
+    #define D_TEXT_SIZE 4//二维码下面的字体大小
     /***************************************
      * 单张规格：
      * 上边距：1.86mm   左边距：1.67mm  二维码到文字的距离：1.7mm 二维码大小：12mmx12mm
